d088591efbaf39af4f776c6d4e5fd0ab66ee09c9
Vatskovskaya <86354815+Vatskovskaya@users.noreply.github.com>
2022-01-04 18:33:29 +0200

    add css and html

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.bromo-slider').slick({
+        speed: 500,
+        fade: true,
+        cssEase: 'linear', 
+        prevArrow:"<button type='button' class='slick-prev pull-left'><img src='../img/icons/prev.png'></img></button>",
+        nextArrow:"<button type='button' class='slick-next pull-right'><img src='../img/icons/next.png'></img></button>"
+    });
+  });
