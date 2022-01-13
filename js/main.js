b1f2ca9c480d35e4d49ebf1055ccf7ee1ad00931
Vatskovskaya <86354815+Vatskovskaya@users.noreply.github.com>
2022-01-13 19:44:47 +0200

    add responsive layout

--- js/main.js
+++ js/main.js
@@ -4,7 +4,17 @@ $(document).ready(function(){
         fade: true,
         cssEase: 'linear', 
         prevArrow:"<button type='button' class='slick-prev pull-left'><img src='../img/icons/prev.png'></img></button>",
-        nextArrow:"<button type='button' class='slick-next pull-right'><img src='../img/icons/next.png'></img></button>"
+        nextArrow:"<button type='button' class='slick-next pull-right'><img src='../img/icons/next.png'></img></button>",
+        responsive: [
+            {
+              breakpoint: 480,
+              settings: {
+                arrows: false,
+                infinite: true,
+                dots: true
+              }
+            }
+        ]
     });
 
     $('.menu-btn').on('click', function(){
